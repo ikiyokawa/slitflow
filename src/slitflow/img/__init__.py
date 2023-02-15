7e2a80da5b9e9283ddceaff6821783670e53c405
Yuma Ito <yitou@bio.titech.ac.jp>
2023-02-15 18:21:28 +0900

    Add img.noise.Gauss

--- src/slitflow/img/__init__.py
+++ src/slitflow/img/__init__.py
@@ -5,5 +5,7 @@ from . import convert
 from . import plot
 from . import create
 from . import calc
+from . import noise
 
-__all__ = ["image", "filter", "montage", "convert", "plot", "create", "calc"]
+__all__ = ["image", "filter", "montage", "convert", "plot", "create", "calc",
+           "noise"]
